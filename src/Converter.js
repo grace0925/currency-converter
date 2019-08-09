6822c6660de044523312cf923141532adf0068b9
GraceLiu <graceliu0925@gmail.com>
2019-08-09 14:28:57 -0400

    🎳 Position swap icon

--- src/Converter.js
+++ src/Converter.js
@@ -1,26 +1,34 @@
 import React, { Component } from 'react'
+import {MdSwapVert} from 'react-icons/md'
 
 class Converter extends Component {
     render() {
         return(
             <div className="card card-body">
                 <h5>1 CAD is equivalent to</h5>
-                <form className="form-inline mt-3 mb-4 mx-auto">
-                    <div className="form-group">
-                        <input className="form-control mx-3"></input>
-                        <select className="form-control">
-                            <option>Option 1</option>
-                        </select>
+                <div className="row justify-content-center">
+                    <div className="col-3 ml-5">
+                        <form className="form-inline mt-3 mb-4 mx-auto">
+                            <div className="form-group">
+                                <input className="form-control mx-3"></input>
+                                <select className="form-control">
+                                    <option>Option 1</option>
+                                </select>
+                            </div>
+                        </form>
+                        <form className="form-inline mb-4">
+                            <div className="form-group">
+                                <input className="form-control mx-3"></input>
+                                <select className="form-control">
+                                    <option>Option 1</option>
+                                </select>
+                            </div>
+                        </form>
                     </div>
-                </form>
-                <form className="form-inline mb-4 mx-auto">
-                    <div className="form-group">
-                        <input className="form-control mx-3"></input>
-                        <select className="form-control">
-                            <option>Option 1</option>
-                        </select>
+                    <div className="col-1 align-self-center">
+                        <MdSwapVert size="45"/>
                     </div>
-                </form>
+                </div>
             </div>
             
         )
