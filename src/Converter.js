d6f6afc71e864dbf3e52fcfc11feb31020713819
GraceLiu <graceliu0925@gmail.com>
2019-08-09 22:09:36 -0400

    🎷 Toggle currency state

--- src/Converter.js
+++ src/Converter.js
@@ -3,20 +3,32 @@ import {MdSwapVert} from 'react-icons/md'
 
 class Converter extends Component {
     state={
-        currencies: ['USD', 'CAD', 'AUD', 'YEN', 'EUR', 'CNH', 'HKD']
+        currencies: ['USD', 'CAD', 'AUD', 'YEN', 'EUR', 'CNH', 'HKD'],
+        baseCurr: 'CAD',
+        baseAmount: '',
+        resultCurr: 'USD',
+        resultAmount: '',
+        date: '',
+    }
+    handleSelect = (e) => {
+        this.setState({
+            [e.target.name]: e.target.value
+        })
     }
     render() {
-        const {currencies} = this.state
+        const {currencies, baseCurr, baseAmount, resultCurr, resultAmount, date} = this.state
         return(
             <div className="row">
                 <div className="card card-body col-6 mx-auto mt-4">
-                    <h5>1 CAD is equivalent to</h5>
+                    <h5>On {date}</h5>
+                    <h6>1 {baseCurr} is equivalent to {resultAmount} {resultCurr}</h6>
                     <div className="row justify-content-center">
                         <div className="col-lg-5">
                             <form className="form-inline mt-3 mb-4 mx-auto">
                                 <div className="form-group">
                                     <input className="form-control mx-3"></input>
-                                    <select className="form-control">
+                                    <select name="baseCurr" value={baseCurr} className="form-control"
+                                            onChange={this.handleSelect}>
                                         {currencies.map(currency => 
                                             <option key={currency} value={currency}>{currency}</option>
                                         )}
@@ -26,7 +38,8 @@ class Converter extends Component {
                             <form className="form-inline mb-4">
                                 <div className="form-group">
                                     <input className="form-control mx-3"></input>
-                                    <select className="form-control">
+                                    <select name="resultCurr" value={resultCurr} className="form-control"
+                                            onChange={this.handleSelect}>
                                         {currencies.map(currency => 
                                             <option key={currency} value={currency}>{currency}</option>    
                                         )}
