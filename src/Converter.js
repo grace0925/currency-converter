007bf17dbf6ebe472bfc1c6dd220971d2bfbfb22
GraceLiu <graceliu0925@gmail.com>
2019-08-11 00:53:25 -0400

    🍓 Fix input bug

--- src/Converter.js
+++ src/Converter.js
@@ -11,6 +11,9 @@ import Flag from 'react-flagkit';
 import IosRefresh from 'react-ionicons/lib/IosRefresh'
 //exchange rate api 
 //https://exchangeratesapi.io/
+// TODO: add tooltips to options
+// TODO: rounding 
+// TODO: better layouts
 
 const useStyles = makeStyles(theme => ({
     card: {
@@ -57,8 +60,7 @@ class ConverterContent extends Component {
     handleInput = (e) => {
         console.log("handle input ")
         this.setState({
-            baseAmount: e.target.value,
-            done: false,
+            baseAmount: e.target.value
         })
         this.calcResult()
     }
@@ -73,15 +75,11 @@ class ConverterContent extends Component {
             resultCurr: baseCurr,
             baseAmount: resultAmount,
             resultAmount: '',
-            done
         })
         process.nextTick(() => {
             this.calcResult()
         })
     }
-    // TODO: migrate to material ui
-    // TODO: add tooltips to options
-    // TODO: emoji
     calcResult = () => {
         const result = this.state.baseAmount
         if (result === isNaN) {
@@ -103,50 +101,51 @@ class ConverterContent extends Component {
         const {currencies, baseCurr, baseAmount, resultCurr, resultAmount, date, done} = this.state
         return (
             <div>
-                {done ? (<CardContent>
-                            <h5>On {date}</h5>
-                            <h4>{baseAmount} {baseCurr} is equivalent to {resultAmount} {resultCurr}</h4>
-                            <div className="row justify-content-center">
-                                <div className="col-lg-5">
-                                    <form className="form-inline mt-3 mb-4 mx-auto">
-                                        <div className="form-group">
-                                            <input type="number" value={baseAmount} step="0.01" min="0.00" placeholder="1.00" className="form-control mx-3" 
-                                                    onChange={this.handleInput}>
-                                            </input>
-                                            <select name="baseCurr" value={baseCurr} className="form-control"
-                                                    onChange={this.handleSelect}>
-                                                {currencies.map(currency => 
-                                                    <option key={currency} value={currency.substring(0,3)}>{currency}</option>
-                                                )}
-                                            </select>
-                                        </div>
-                                    </form>
-                                    <form className="form-inline mb-4">
-                                        <div className="form-group">
-                                            <input disabled={true} value={resultAmount} className="form-control mx-3"></input>
-                                            <select name="resultCurr" value={resultCurr} className="form-control"
-                                                    onChange={this.handleSelect}>
-                                                {currencies.map(currency => 
-                                                    <option key={currency} value={currency}>{currency}</option>    
-                                                )}
-                                            </select>
-                                        </div>
-                                    </form>
+                {done ? (
+                <CardContent>
+                    <h5>On {date}</h5>
+                    <h4>{baseAmount} {baseCurr} is equivalent to {resultAmount} {resultCurr}</h4>
+                    <div className="row justify-content-center">
+                        <div className="col-lg-5">
+                            <form className="form-inline mt-3 mb-4 mx-auto">
+                                <div className="form-group">
+                                    <input type="number" value={baseAmount} step="0.01" min="0.00" placeholder="1.00" className="form-control mx-3" 
+                                            onChange={this.handleInput}>
+                                    </input>
+                                    <select name="baseCurr" value={baseCurr} className="form-control"
+                                            onChange={this.handleSelect}>
+                                        {currencies.map(currency => 
+                                            <option key={currency} value={currency.substring(0,3)}>{currency}</option>
+                                        )}
+                                    </select>
                                 </div>
-                                <div className="col-lg-2 ml-5 align-self-center mb-2">
-                                    <div className="row mb-1">
-                                        <Flag country={this.state.baseCurr.substring(0,2)}/>
-                                    </div>
-                                    <div className="row">
-                                        <MdSwapVert onClick={this.handleSwap} size="35"/>
-                                    </div>
-                                    <div className="row">
-                                        <Flag country={this.state.resultCurr.substring(0,2)}/>
-                                    </div>
+                            </form>
+                            <form className="form-inline mb-4">
+                                <div className="form-group">
+                                    <input disabled={true} value={resultAmount} className="form-control mx-3"></input>
+                                    <select name="resultCurr" value={resultCurr} className="form-control"
+                                            onChange={this.handleSelect}>
+                                        {currencies.map(currency => 
+                                            <option key={currency} value={currency}>{currency}</option>    
+                                        )}
+                                    </select>
                                 </div>
+                            </form>
+                        </div>
+                        <div className="col-lg-2 ml-5 align-self-center mb-2">
+                            <div className="row mb-1">
+                                <Flag country={this.state.baseCurr.substring(0,2)}/>
                             </div>
-                        </CardContent>) 
-                : (<IosRefresh fontSize="60px" color="#347eff" rotate={true} />)}
+                            <div className="row">
+                                <MdSwapVert onClick={this.handleSwap} size="35"/>
+                            </div>
+                            <div className="row">
+                                <Flag country={this.state.resultCurr.substring(0,2)}/>
+                            </div>
+                        </div>
+                    </div>
+                </CardContent>) 
+            : (<IosRefresh fontSize="60px" color="#347eff" rotate={true} />)}
             </div>
         ) 
     }
